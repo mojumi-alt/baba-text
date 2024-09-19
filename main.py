a7c12dd8edeb9a739fa228ae872bbaf71ed22547
Moritz Milla <80782057+mojumi-alt@users.noreply.github.com>
2024-09-19 22:11:01 +0200

    Work around cloudformation issues

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from baba_text.animated_text import AnimatedText
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         raise ValueError(
-            f"Expected 2 arguments (input string to convert and output file name), got {len(sys.argv)} instead!"
+            f"Expected 2 arguments (input string to convert and output file name), got {len(sys.argv) - 1} instead!"
         )
 
     AnimatedText(sys.argv[1]).write_to_gif(sys.argv[2])
